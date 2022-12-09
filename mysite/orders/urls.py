e72b23b6b74030cdae4244e569530e5130865960
Donatas Noreika <donoras@gmail.com>
2022-12-09 10:32:51 +0200

    +Padaryti prisijungusio vartotojo užsakymų atvaizdavimo puslapį (listview)

--- mysite/orders/urls.py
+++ mysite/orders/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import OrderListView
+from .views import OrderListView, UserOrderListView
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="orders"),
+    path("orders/", UserOrderListView.as_view(), name="user_orders"),
 ]
--- mysite/orders/views.py
+++ mysite/orders/views.py
@@ -7,3 +7,13 @@ class OrderListView(generic.ListView):
     model = Order
     template_name = 'orders.html'
     context_object_name = 'orders'
+
+class UserOrderListView(generic.ListView):
+    model = Order
+    template_name = 'user_orders.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+

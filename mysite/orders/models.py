06a92cec208041b94d6c588d17db88ce9d0bdba7
Donatas Noreika <donoras@gmail.com>
2022-12-09 09:24:56 +0200

    +Sukurti projekto modelius

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField("Pavadinimas", max_length=200)
+    price = models.FloatField("Kaina")
+
+class Status(models.Model):
+    name = models.CharField("Pavadinimas", max_length=200)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField("Data", auto_now_add=True)
+    status = models.ForeignKey("Status", on_delete=models.SET_NULL, null=True, blank=True)
+
+class OrderLine(models.Model):
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.SET_NULL, null=True, blank=True)
+    qty = models.IntegerField("Kiekis")
